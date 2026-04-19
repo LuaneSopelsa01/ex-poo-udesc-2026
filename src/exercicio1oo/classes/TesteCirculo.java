076a6d73613a52910384be054b644223e6b9f9e9
Luane Sopelsa <09042731990@edu.udesc.br>
2026-04-19 18:36:39 -0300

    Exercicio 1 - Classe circulo e respectivo teste

---
+++ src/exercicio1oo/classes/TesteCirculo.java
@@ -0,0 +1,17 @@
+package exercicio1oo.classes;
+
+import java.util.Scanner;
+
+public class TesteCirculo {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Circulo um = new Circulo();
+
+        System.out.print("Raio: ");
+        um.raio = leitor.nextDouble();
+
+        System.out.println("Dados do Circulo");
+        System.out.println("               ");
+        System.out.println("Raio:    " + um.raio);
+    }
+}
